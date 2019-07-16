fb563dd48447112ee91ca9085e5ca5d1f6cba3c5
James Phillips <james.phillips@concentra.co.uk>
2019-07-16 10:38:56 +0100

    Structure

--- src/main/scala/example/Security.scala
+++ src/main/scala/example/Security.scala
@@ -1,1 +1,21 @@
 package example
+
+trait T
+trait A
+
+object Utility {
+  def combine(a1: A, a2: A): A = a1
+}
+
+import Utility.combine
+
+case class Node(
+  left: Option[Node],
+  right: Option[Node],
+  predicate: Option[T => Boolean],
+  actions: A
+) {
+
+  def reduce(t: T, a: A): A = ???
+
+}
